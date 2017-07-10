99a1ae062dddadca880b85c1faaf63d7ffb66604
Maria Huerta <Malena@mariahuertasMBP.attlocal.net>
2017-07-09 21:38:01 -0700

    Have conditions for going over guessed number and also tested if equal to guess number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,11 +21,34 @@ var random;
 
 $(document).ready(function(){
 
-	random=Math.floor(Math.random() * 13);
+	//random number that will go to each image
+	random=Math.floor(Math.random() * (13-1+1))+1;
+
+	//created random number for user to guess 
+	var guessRandom= Math.floor(Math.random() * (100-40+1))+40;
+
+	//Updates the title or user guess header
+	$("#guess").text(guessRandom);
 
 	// create an image then get it a random function
 	$("#summer, #morty, #rickSanchez").on("click",function(){
-		alert("you clicked " + counter + " this many times");
-		counter += random;		
+
+		//Everytime user clicks the pictures it starts a counter
+		counter += random;	
+
+		console.log("you clicked " + counter + " this many times");
+
+		//Created conditions for going over the Guess number
+		if(guessRandom === counter){
+
+			alert("You win");
+		}
+		else if(counter > guessRandom ){
+
+			alert("you lose");
+		}
 	});
 });
+
+
+// var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
